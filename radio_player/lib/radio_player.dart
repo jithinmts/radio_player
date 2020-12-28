280c657dc354747941d67eef1da19cca9521e99b
cheebeez <xc@yar.net>
2020-12-28 20:50:15 +0300

    Remove platformVersion method

--- radio_player/lib/radio_player.dart
+++ radio_player/lib/radio_player.dart
@@ -1,14 +1,25 @@
+/*
+ *  radio_player.dart
+ *
+ *  Created by Ilya Chirkunov <xc@yar.net> on 28.12.2020.
+ */
 
 import 'dart:async';
-
 import 'package:flutter/services.dart';
 
 class RadioPlayer {
-  static const MethodChannel _channel =
-      const MethodChannel('radio_player');
+  final String streamTitle;
+  final String streamCover;
+  final String streamURL;
+  static const MethodChannel _channel = const MethodChannel('radio_player');
 
-  static Future<String> get platformVersion async {
-    final String version = await _channel.invokeMethod('getPlatformVersion');
-    return version;
+  RadioPlayer(this.streamTitle, this.streamCover, this.streamURL);
+
+  Future<void> play() async {
+    await _channel.invokeMethod('play');
+  }
+
+  Future<void> pause() async {
+    await _channel.invokeMethod('pause');
   }
 }
