a1a3d44b4d39e00ee7fb801d4082e4a2a2185d60
cheebeez <xc@yar.net>
2021-01-04 06:25:57 +0300

    Move "init" method to constructor

--- radio_player/lib/radio_player.dart
+++ radio_player/lib/radio_player.dart
@@ -7,13 +7,13 @@
 import 'dart:async';
 import 'package:flutter/services.dart';
 
-/// Plays a radio stream using AVPlayer for iOS and MediaPlayer for Android.
+/// Plays a radio stream using AVPlayer for iOS and ExoPlayer for Android.
 /// Supports the playback state and metadata stream.
 class RadioPlayer {
-  static const MethodChannel _channel = const MethodChannel('radio_player');
+  static const MethodChannel _channel = MethodChannel('radio_player');
 
-  Future<void> init(String streamTitle, String streamURL) async {
-    await _channel.invokeMethod('init');
+  RadioPlayer(String streamTitle, String streamUrl) {
+    _channel.invokeMethod('init', <dynamic>[streamTitle, streamUrl]);
   }
 
   Future<void> play() async {
