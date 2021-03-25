ca8fd31defd74c6082982eaeeffe2732fa73fb10
Ilya Chirkunov <xc@yar.net>
2021-03-26 02:14:20 +0300

    Update radio_player_test.dart

--- test/radio_player_test.dart
+++ test/radio_player_test.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:radio_player/radio_player.dart';
+//import 'package:radio_player/radio_player.dart';
 
 void main() {
   const MethodChannel channel = MethodChannel('radio_player');
